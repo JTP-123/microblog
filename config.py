674235622f3277844f097cbd4c79f56255da585f
JTP-123 <68136548+JTP-123@users.noreply.github.com>
2023-12-30 12:32:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, os.environ.get('DATABASE_FILE', 'data.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, os.environ.get('DATABASE_FILE', 'data.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     # 配置email
     MAIL_SERVER = os.environ.get('MAIL_SERVER') 
