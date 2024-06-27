2d65829d045035c52a0e3ec18639b3072dd8f217
JTP-123 <68136548+JTP-123@users.noreply.github.com>
2024-06-27 11:20:10 +0800

    Update routes.py
    fix upload bug

--- app/main/routes.py
+++ app/main/routes.py
@@ -205,7 +205,8 @@ def delete_comment(id):
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in current_app.config['ALLOWED_EXTENSIONS']
-
+    
+current_app.config['UPLOAD_FOLDER'] = "home/JTPing/microblog/upload_folder"
 @bp.route('/upload', methods=['GET', 'POST'])
 @login_required
 def upload():
